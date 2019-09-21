f589b7e0b01d49628b3dac76c7c0afc8b77b4e4a
Tomasz Murglin <tomasz.murglin@gmail.com>
2019-09-21 11:42:51 +0200

    hidden secrets

--- src/main/java/com/murglin/consulting/diagram/Publisher.java
+++ src/main/java/com/murglin/consulting/diagram/Publisher.java
@@ -6,9 +6,9 @@ import com.structurizr.api.StructurizrClientException;
 
 public class Publisher {
 
-    private static final String API_KEY = "26bae340-a30d-42c4-9d62-973b262b32e9";
-    private static final String API_SECRET = "e5a81a4f-fd84-4d9f-80b2-8241999d1c4f";
-    private static final long WORKSPACE_ID = 46905;
+    private static final String API_KEY = "key";
+    private static final String API_SECRET = "secret";
+    private static final long WORKSPACE_ID = -1;
 
     public static void publishToDiagramStructurizr(final Workspace workspace) throws StructurizrClientException {
         StructurizrClient structurizrClient = new StructurizrClient((API_KEY), API_SECRET);
